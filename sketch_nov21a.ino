ca0cb05b958802a6dd30267e83e04c73f5badcc0
Abdul Ahad <87071231+AbdulAhad11@users.noreply.github.com>
2021-11-30 18:48:28 +0500

    Add files via upload

---
+++ sketch_nov21a.ino
@@ -0,0 +1,204 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+
+#include "index.h"
+
+#define ENA   14          // Enable/speed motors Right        GPIO14(D5)
+#define ENB   12          // Enable/speed motors Left         GPIO12(D6)
+#define IN_1  15          // L298N in1 motors Rightx          GPIO15(D8)
+#define IN_2  13          // L298N in2 motors Right           GPIO13(D7)
+#define IN_3  2           // L298N in3 motors Left            GPIO2(D4)
+#define IN_4  0           // L298N in4 motors Left            GPIO0(D3)
+#include <Servo.h>
+
+Servo servo;
+
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+
+String command;             //String to store app command state.
+int speedCar = 800;         // 400 - 1023.
+int speed_Coeff = 3;
+
+const char* ssid = "Abdul Ahad";
+const char* password = "abdulahad";
+ESP8266WebServer server(80);
+
+void setup() {
+ 
+ pinMode(ENA, OUTPUT);
+ pinMode(ENB, OUTPUT);  
+ pinMode(IN_1, OUTPUT);
+ pinMode(IN_2, OUTPUT);
+ pinMode(IN_3, OUTPUT);
+ pinMode(IN_4, OUTPUT); 
+  Serial.begin(9600);
+
+   servo.attach(4); //D2
+
+servo.write(0);
+
+delay(2000);
+
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+ 
+  server.on("/", handleRoot);      //Which routine to handle at root location
+
+  server.begin();                  //Start server
+  Serial.println("HTTP server started");  
+}
+
+void goAhead(){ 
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+  }
+
+void goBack(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goRight(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+  }
+
+void goLeft(){
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goAheadRight(){
+      
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar/speed_Coeff);
+ 
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+   }
+
+void goAheadLeft(){
+      
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar/speed_Coeff);
+  }
+
+void goBackRight(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar/speed_Coeff);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goBackLeft(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar/speed_Coeff);
+  }
+
+void stopRobot(){  
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+ }
+
+void loop() {
+  servo.write(180);
+
+delay(1000);
+
+servo.write(0);
+
+delay(1000);
+    server.handleClient();
+    
+      command = server.arg("State");
+      if (command == "F") goAhead();
+      else if (command == "B") goBack();
+      else if (command == "L") goLeft();
+      else if (command == "R") goRight();
+      else if (command == "I") goAheadRight();
+      else if (command == "G") goAheadLeft();
+      else if (command == "J") goBackRight();
+      else if (command == "H") goBackLeft();
+      else if (command == "0") speedCar = 400;
+      else if (command == "1") speedCar = 470;
+      else if (command == "2") speedCar = 540;
+      else if (command == "3") speedCar = 610;
+      else if (command == "4") speedCar = 680;
+      else if (command == "5") speedCar = 750;
+      else if (command == "6") speedCar = 820;
+      else if (command == "7") speedCar = 890;
+      else if (command == "8") speedCar = 960;
+      else if (command == "9") speedCar = 1023;
+      else if (command == "S") stopRobot();
+}
+
+void handleRoot() {
+ String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
+}
---
+++ style.h
@@ -0,0 +1,8 @@
+{
+    background-image: url("https://images.pexels.com/photos/1335077/pexels-photo-1335077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500");
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-size: 1900px;
+    opacity: 30;
+};
